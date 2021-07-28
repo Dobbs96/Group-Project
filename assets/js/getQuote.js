0168f0b1060d5cfc1153a3f0360e297102b75946
James Dobbs <dobbs282@gmail.com>
2021-07-28 12:09:30 -0400

    fixed random array

--- assets/js/getQuote.js
+++ assets/js/getQuote.js
@@ -1,18 +1,15 @@
 var inspitableURL = "https://type.fit/api/quotes";
 var quote = $("#quote");
 var quotesAuthor = $("#quote-author");
-
 function getQuote() {
   fetch(inspitableURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-      quote.text(`"${data[Math.floor(Math.random() * data.length)].text}"`);
-      quotesAuthor.text(
-        `"${data[Math.floor(Math.random() * data.length)].author}"`
-      );
+      var randomArr = Math.floor(Math.random() * data.length);
+      quote.text(`"${data[randomArr].text}"`);
+      quotesAuthor.text(`"${data[randomArr].author}"`);
     });
 }
 
